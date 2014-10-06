a9d648317dd421a998b40b6fc57757ac0adbed6a
Adam Crews <adam@pathdefender.com>
2014-10-06 15:27:22 -0700

    fix vars

--- manifests/package.pp
+++ manifests/package.pp
@@ -6,21 +6,23 @@ class hacme::package (
   
 ) inherits hacme::params { 
 
+  $base_zip = "${hacme_base}/${hacme_pkg_zip}"
+  $full_url = "${hacme_src_url}/${hacme_pkg_zip}"
+
   file { $hacme_base:
     ensure => directory,
   }
 
   exec { 'Download Hacme':
     #command   => "powershell.exe -Command (new-object System.Net.WebClient).DownloadFile(\'${hacme_src_url}/${hacme_pkg_zip}\', \'C:\\hacme\\${hacme_pkg_zip}\')",
-    command   => "powershell.exe -Command (new-object System.Net.WebClient).DownloadFile(\'${hacme_src_url}/${hacme_pkg_zip}\', \'${hacme_base}/${hacme_pkg_zip}\')",
+    command   => "powershell.exe -Command (new-object System.Net.WebClient).DownloadFile(\'${full_url}\', \'${base_zip}\')",
     path      => 'C:\Windows\System32\WindowsPowerShell\v1.0',
-    #creates   => "C:\\hacme\\${hacme_pkg_zip}",
-    creates   => "${hacme_base}/${hacme_pkg_zip}",
+    creates   => $base_zip,
     logoutput => true,
   }
 
   exec { 'Unzip Hacme':
-    command     => "powershell.exe -Command (new-object -com shell.application).namespace(\'${hacme_base}\').CopyHere((new-object -com shell.application).namespace(\'${hacme_base}/${hacme_pkg_zip}\').Items(),16))",
+    command     => "powershell.exe -Command (new-object -com shell.application).namespace(\'${hacme_base}\').CopyHere((new-object -com shell.application).namespace(\'${base_zip}\').Items(),16)",
     path        => 'C:\Windows\System32\WindowsPowerShell\v1.0',
     logoutput   => true,
     subscribe   => Exec['Download Hacme'],
