008b36604251f5a09e436c308bd66784395b7a59
Adam Crews <adam@pathdefender.com>
2014-10-06 15:44:57 -0700

    typo

--- manifests/package.pp
+++ manifests/package.pp
@@ -22,7 +22,8 @@ class hacme::package (
   }
 
   exec { 'Unzip Hacme':
-    command     => "powershell.exe -Command (new-object -com shell.application).namespace(\'${hacme_base}\').CopyHere((new-object -com shell.application).namespace(\'${base_zip}\').Items(),16)",
+    #    command => "powershell.exe -Command (new-object -com shell.application).namespace(\'${hacme_base}\').CopyHere((new-object -com shell.application).namespace(\'${base_zip}\').Items(),16)",
+    command => "powershell.exe -Command (new-object -com shell.application).namespace(\'${hacme_base}\').CopyHere((new-object -com shell.application).namespace(\'${base_zip}\').Items(),16)",
     path        => 'C:\Windows\System32\WindowsPowerShell\v1.0',
     logoutput   => true,
     subscribe   => Exec['Download Hacme'],
